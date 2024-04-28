ad46929a14df5ffbed53463a21ee77b5f2ff28fe
GottJesus <richterpaul@freenet.de>
2024-04-28 12:19:10 +0200

    Version 0.0.3

--- index.php
+++ index.php
@@ -6,12 +6,5 @@
 		<h3>Hallo Versuch</h3>
 		<h3>Hallo Versuch</h3>
 		<h3>Hallo Versuch</h3>
-		<h3>Hallo Versuch</h3>
-		<h3>Hallo Versuch</h3>
-		<h3>Hallo Versuch</h3>
-		<h3>Hallo Versuch</h3>
-		<h3>Hallo Versuch</h3>
-		<h3>Hallo Versuch</h3>
-		<h3>Hallo Versuch</h3>
-	</body>
+		<h3>Hallo Versuch</h3>	</body>
 </html>
